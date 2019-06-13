832b16dda252c4e825c67d791e574ed63da19c82
patrickbrown <patricksbrown1992@gmail.com>
2019-06-12 20:34:32 -0400

    update basic butterfree class

---
+++ js/butterfree.js
@@ -0,0 +1,11 @@
+class Butterfree{
+    constructor(x, y, health){
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.height = 17;
+        this.width = 21;
+
+       
+    }
+}
