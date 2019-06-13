ead46328699b1ae87bf20510654b996f0be04d9f
patrickbrown <patricksbrown1992@gmail.com>
2019-06-13 14:47:09 -0400

    add music and game file

---
+++ js/game.js
@@ -0,0 +1,11 @@
+class Game{
+    constructor(){
+
+        this.audio = new Audio("assets/pokemon-trainer-battle-music.mp3");
+        this.audio.loop = true;
+        this.audio.volume = .25;
+        this.auto.load();
+    }
+
+}
+
