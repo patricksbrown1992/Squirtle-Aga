ecb40fd67307cb94e18446c1b515ee6387b2bb67
patrickbrown <patricksbrown1992@gmail.com>
2019-06-12 20:35:01 -0400

    create basic dragonite class

---
+++ js/dragonite.js
@@ -0,0 +1,10 @@
+class Dragonite{
+    constructor(x, y, health){
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.height = 31;
+        this.width = 24;
+          
+    }
+}
