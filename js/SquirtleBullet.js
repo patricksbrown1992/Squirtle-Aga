9a64412187e8e9790a0b0da5f052cd875083ff31
patrickbrown <patricksbrown1992@gmail.com>
2019-06-13 17:20:55 -0400

    pre pages push

--- js/SquirtleBullet.js
+++ js/SquirtleBullet.js
@@ -1,10 +1,11 @@
 class SquirtleBullet{
-    constructor(x,y){
+    constructor(x,y, dy){
         this.x = x;
         this.y = y;
         this.width = 12;
         this.height = 25;
-        this.dy = y -= 25;
+        this.dy = dy;
+        this.speed = 25;
         // this.speed = 25;
         this.color = "green";
     }
