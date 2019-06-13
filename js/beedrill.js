ee1f5c66156f410e518d36a8963fdc199182832a
patrickbrown <patricksbrown1992@gmail.com>
2019-06-12 20:34:05 -0400

    update basic beedrill class

---
+++ js/beedrill.js
@@ -0,0 +1,10 @@
+class Beedrill{
+    constructor(x, y, health){
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.height = 22;
+        this.width = 20;
+
+    }
+}
