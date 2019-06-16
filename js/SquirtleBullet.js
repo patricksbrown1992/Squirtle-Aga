c3fefb8ecfd7e321793f1af818395935d9317af0
patrickbrown <patricksbrown1992@gmail.com>
2019-06-16 13:42:49 -0400

    update Squirtle Bullet constructor

--- js/SquirtleBullet.js
+++ js/SquirtleBullet.js
@@ -1,11 +1,21 @@
+import Squirtle from './squirtle';
+
 class SquirtleBullet{
-    constructor(x,y, dy){
-        this.x = x;
-        this.y = y;
-        this.width = 12;
-        this.height = 25;
-        this.dy = dy;
-        this.speed = 25;
+    constructor(canvas, ctx, Squirtle){
+        
+        this.sBulletWidth = 3;
+        this.sBulletHeight = 25;
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.sBulletHealth = 1;
         this.color = "green";
+        this. draw = this.draw.bind(this);
+        this.drawBullet = this.draw.bind(this);
+        this.x = Squirtle.squirtleX;
+        this.y = this.canvas.height - (Squirtle.squirtleHeight * 2);
+        this.speed = 3;
     }
+
+
+
 }
