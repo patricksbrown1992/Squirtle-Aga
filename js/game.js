e921fe7e1e10862fd6b7dfa43d6d4624861aa1c5
patrickbrown <patricksbrown1992@gmail.com>
2019-06-27 14:57:01 -0400

    loads winning photo

--- js/game.js
+++ js/game.js
@@ -1,13 +1,14 @@
 
 
 class Game{
-    constructor(canvas, ctx, Squirtle, squirtlePhoto, Butterfree, butterfreePhoto, Beedrill, beedrillPhoto, SquirtleBulletShooter, EnemyBulletShooter, gameOverPhoto){
+    constructor(canvas, ctx, Squirtle, squirtlePhoto, Butterfree, butterfreePhoto, Beedrill, beedrillPhoto, SquirtleBulletShooter, EnemyBulletShooter, gameOverPhoto, dealWithItPhoto){
         this.canvas = canvas;
         this.ctx = ctx;
         this.squirtlePhoto = squirtlePhoto;
         this.gameOverPhoto = gameOverPhoto;
         this.butterfreePhoto = butterfreePhoto;
         this.beedrillPhoto = beedrillPhoto;
+        this.dealWithItPhoto = dealWithItPhoto;
         this.Squirtle = new Squirtle(this.canvas, this.ctx, this.squirtlePhoto);
         this.Butterfree = new Butterfree(this.canvas, this.ctx, this.butterfreePhoto);
         this.Beedrill = new Beedrill(this.canvas, this.ctx, this.beedrillPhoto);
@@ -27,40 +28,74 @@ class Game{
 
     }
     life(){
-        // debugger
+        debugger
         if(this.Squirtle.health <= 0){
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.ctx.drawImage(this.gameOverPhoto, 0, 0, 500, 200)
         } else {
 
-            requestAnimationFrame(this.life)
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-            this.Squirtle.draw();
-            this.Butterfree.draw();
-            this.Beedrill.draw();
-            this.SquirtleBulletShooter.bullets.forEach((bullet) => {
-                bullet.draw();
-                this.SquirtleBulletShooter.collisionDetection();
-                this.SquirtleBulletShooter.bullets = this.SquirtleBulletShooter.bullets.filter(bullet => bullet.y > 0);
-            });
+            let count = 0;
+            this.Beedrill.beedrills.forEach( (beedrill) => {
+                if(beedrill[0].health > 0){
+                    count += 1 ;
+                }
+                if (beedrill[1].health > 0) {
+                    count += 1;
+                }
+            })
+    
+            this.Butterfree.butterfrees.forEach( (butterfree) => {
+            
+                if(butterfree[0].health > 0){
+                    count += 1;
+                }
+                if (butterfree[1].health > 0) {
+                    count += 1;
+                }
+            }) 
             // debugger
-            setInterval(this.EnemyBulletShooter.shooterCheck(), 2000);
-            this.EnemyBulletShooter.bullets.forEach((bullet) => {
-                bullet.draw();
-              
+            if( count == 0){
+                // debugger
+                const image = new Image()
+                image.src = './assets/deal-with-it.jpg';
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                debugger
+                image.onload = () => {
+                    this.ctx.drawImage(image, 0, 0, 500, 339, 0, 0, this.canvas.width, this.canvas.height);
+                }
                 
-                this.EnemyBulletShooter.collisionDetection();
-                this.EnemyBulletShooter.bullets = this.EnemyBulletShooter.bullets.filter(bullet => bullet.y < 540)
-            });
+                
+            } else {
+    
+                requestAnimationFrame(this.life)
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+                this.Squirtle.draw();
+                this.Butterfree.draw();
+                this.Beedrill.draw();
+                this.SquirtleBulletShooter.bullets.forEach((bullet) => {
+                    bullet.draw();
+                    this.SquirtleBulletShooter.collisionDetection();
+                    this.SquirtleBulletShooter.bullets = this.SquirtleBulletShooter.bullets.filter(bullet => bullet.y > 0);
+                });
+                // debugger
+                setInterval(this.EnemyBulletShooter.shooterCheck(), 2000);
+                this.EnemyBulletShooter.bullets.forEach((bullet) => {
+                    bullet.draw();
+                  
+                    
+                    this.EnemyBulletShooter.collisionDetection();
+                    this.EnemyBulletShooter.bullets = this.EnemyBulletShooter.bullets.filter(bullet => bullet.y < 540)
+                });
+            }
+    
+     
         }
-
- 
       
     }
 
 
     begin(){
- 
+        
         this.Squirtle.draw();
         this.Butterfree.draw();
         this.Beedrill.draw();
