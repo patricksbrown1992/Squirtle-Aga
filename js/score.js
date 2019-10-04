b897d103ca926d0f93389b8683d07bb688fd4eed
patrickbrown <patricksbrown1992@gmail.com>
2019-10-04 14:53:26 -0400

    better score color

--- js/score.js
+++ js/score.js
@@ -18,7 +18,7 @@ class Score {
          this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
          this.ctx.fill();
          this.ctx.font = "20px Arial";
-         this.ctx.fillStyle = "red";
+         this.ctx.fillStyle = "skyblue";
          this.ctx.textAlign = "top";
          this.ctx.fillText(`Lives: ${this.Squirtle.health}`, this.canvas.width - 100, 15);
          this.ctx.fillText(`Score: ${this.Squirtle.score}`, 100, 15);
