bd3694fe1558b1b0ea529f5e275c1f34e2ca19ac
patrickbrown <patricksbrown1992@gmail.com>
2019-10-18 23:52:45 -0400

    better styling

--- js/game.js
+++ js/game.js
@@ -224,20 +224,29 @@ class Game{
     }
 
     menuDraw() {
-        this.ctx.fillStyle = "steelblue";
         this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.fill();
-        this.ctx.font = "40px Arial";
-        this.ctx.fillStyle = "gold";
-        this.ctx.textAlign = "center";
-        this.ctx.fillText("Press Enter to begin", this.canvas.width / 2, 100);
-        this.ctx.fillText("Press Z to shoot", this.canvas.width / 2, 150);
-        this.ctx.fillText("You have 3 bullets at a time", this.canvas.width / 2, 200);
-        this.ctx.fillText("Press left and right to move", this.canvas.width / 2, 250);
-        this.ctx.fillText("Press P to pause", this.canvas.width / 2, 300);
-        this.ctx.fillText("Press M to mute", this.canvas.width / 2, 350);
-
+        // this.ctx.font = "40px Arial";
+        // this.ctx.textAlign = "center";
+        // this.ctx.fillText("Press Enter to begin", this.canvas.width / 2, 100);
+        // this.ctx.fillText("Press Z to shoot", this.canvas.width / 2, 150);
+        // this.ctx.fillText("You have 3 bullets at a time", this.canvas.width / 2, 200);
+        // this.ctx.fillText("Press left and right to move", this.canvas.width / 2, 250);
+        // this.ctx.fillText("Press P to pause", this.canvas.width / 2, 300);
+        // this.ctx.fillText("Press M to mute", this.canvas.width / 2, 350);
         
+        // let press_m = new Image();
+        let press_enter = new Image();
+        let lives_photo = new Image();
+        press_enter .onload = () => {
+            this.ctx.fillStyle = "steelblue";
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+            this.ctx.drawImage(press_enter, 0, 0, this.canvas.width, 100);
+            this.ctx.drawImage(lives_photo, 0, 100, this.canvas.width, 150);
+            this.ctx.fillStyle = "gold";
+        }
+        press_enter.src = './assets/press-enter.png';
+        lives_photo.src = './assets/3-lives.png';
     }
     fainted(){
         
@@ -256,6 +265,7 @@ class Game{
 
 
         let img = new Image();
+
         img.onload = () => {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
             this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
